5ef080d8bafe521b8d3d42e37b1edfcd0761803c
resolvewang <1796246076@qq.com>
2017-05-30 20:10:51 +0800

    增加sqlalchemy的sql调试语句

--- app/main.py
+++ app/main.py
@@ -5,4 +5,5 @@ from app.base import MySQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 db = MySQLAlchemy(app)
+app.config['SQLALCHEMY_ECHO'] = False
 app.wsgi_app = ProxyFix(app.wsgi_app)
