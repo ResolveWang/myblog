120cfe8f967070cdeb61839757fdda4a3d06b362
resolvewang <1796246076@qq.com>
2017-05-31 09:54:22 +0800

    init

---
+++ admin/static/js/other.js
@@ -0,0 +1,42 @@
+/**
+ * Created by rookiecoder on 2016/6/7.
+ */
+$(function(){
+    //得到焦点
+    $("#password").focus(function(){
+        $("#left_hand").animate({
+            left: "150",
+            top: " -38"
+        },{step: function(){
+            if(parseInt($("#left_hand").css("left"))>140){
+                $("#left_hand").attr("class","left_hand");
+            }
+        }}, 2000);
+        $("#right_hand").animate({
+            right: "-64",
+            top: "-38px"
+        },{step: function(){
+            if(parseInt($("#right_hand").css("right"))> -70){
+                $("#right_hand").attr("class","right_hand");
+            }
+        }}, 2000);
+    });
+    //失去焦点
+    $("#password").blur(function(){
+        $("#left_hand").attr("class","initial_left_hand");
+        $("#left_hand").attr("style","left:100px;top:-12px;");
+        $("#right_hand").attr("class","initial_right_hand");
+        $("#right_hand").attr("style","right:-112px;top:-12px");
+    });
+});
+var sub = function () {
+    var keyword = $('.form-inline .input-default').val().replace(/^\s*/, '');
+    if (keyword === '') {
+        if ($('#notice').length === 0) {
+            $('.form-inline').append('<div id="notice"><span style="color:red;">输入不能为空</span></div>');
+        }
+        return false;
+    } else {
+        return true;
+    }
+};
